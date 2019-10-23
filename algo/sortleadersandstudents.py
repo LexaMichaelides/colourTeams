6f52c822d7bfbbd5862a29bda95c3bfe9f5416fa
Markus Hamann <mhamann@edu.uwaterloo.ca>
2019-10-23 11:01:58 -0400

    Combining the student and leader sortation files

---
+++ algo/sortleadersandstudents.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd 
+
+class sortingalgo:
+    
+    def create_leader_groups(path):
+        inputdf = pd.read_csv(path)
+
+        #replace "U" and "A" genders with a random choice of {"F", "M"}
+        genders = ["M", "F"]
+        inputdf['gender'] = inputdf['gender'].replace("U", np.random.choice(genders, 1)[0])
+        inputdf['gender'] = inputdf['gender'].replace("A", np.random.choice(genders, 1)[0])
+
+        #program bins
+        arch = ["AE", "ARCH"]
+        civ = ["CIVE", "GEO", "ENV"]
+        ece = ["COMP", "ECE"]
+        mech = ["MECH", "TRON"]
+
+        bins = [arch, civ, ece, mech]
+
+        #use anonymous function to rename program to their parent programs concatenated by "/"
+        for bin in bins:
+            inputdf['program'] = inputdf.apply(lambda x: "/".join(bin) if x['program'] in bin else x['program'], axis=1)
+
+        programs = ['AE/ARCH', 'BME', 'CHE', 'CIVE/GEO/ENV', 'COMP/ECE', 'MECH/TRON',
+            'MGTE', 'NANO', 'SE', 'SYDE']
+
+        groups = range(1,19)
+
+        df = inputdf
+
+        #assign leaders based on rand unif distribution [1,19)
+        df['group'] = np.random.randint(1, 19, df.shape[0]) 
+
+        return df
+
+
+    def create_student(path):
+
+        #read in data from CSV file 
+        data = pd.read_csv(path)
+
+        data['gender'] = data['gender'].replace('A','F')
+        data['gender'] = data['gender'].replace('U','F')
+
+        df = data
+
+        df['group'] = np.random.randint(1, 19, df.shape[0])
+
+        summary = df.groupby(['group', 'gender']).size().reset_index(name='counts')
+        
+        return df
