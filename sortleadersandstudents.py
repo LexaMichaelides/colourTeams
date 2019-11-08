f4ced9a5999141bda435c1e6dd115b99275a05f9
SunriseLong <44146580+SunriseLong@users.noreply.github.com>
2019-11-08 15:15:31 -0500

    Anti requests (#18)
    * remove unused code
    
    * add 1 anti-request functionality
    
    * add num_groups variable
    
    * drop  column in return dataframe

--- sortleadersandstudents.py
+++ sortleadersandstudents.py
@@ -5,6 +5,8 @@ import os
 def create_leader_groups(path):
     inputdf = pd.read_csv(path)
 
+    num_groups = 18
+    
     #replace "U" and "A" genders with a random choice of {"F", "M"}
     genders = ["M", "F"]
     inputdf['gender'] = inputdf['gender'].replace("U", np.random.choice(genders, 1)[0])
@@ -22,17 +24,39 @@ def create_leader_groups(path):
     for bin in bins:
         inputdf['program'] = inputdf.apply(lambda x: "/".join(bin) if x['program'] in bin else x['program'], axis=1)
 
-    programs = ['AE/ARCH', 'BME', 'CHE', 'CIVE/GEO/ENV', 'COMP/ECE', 'MECH/TRON',
-        'MGTE', 'NANO', 'SE', 'SYDE']
-
-    groups = range(1,19)
-
     df = inputdf
 
     #assign leaders based on rand unif distribution [1,19)
     df['group'] = np.random.randint(1, 19, df.shape[0]) 
 
-    return df
+    #anti requests
+    df['name'] = df['last_name'] + ", " + df['first_name']
+
+    antiDF = df[df['anti1'].notnull()]
+    antiDF['anti1group'] = ""
+
+    #get the group that your anti request belongs
+    for i, row in antiDF.iterrows():
+        #searches df for name and fetches their respective group
+        antiDF['anti1group'][i] = df[df['name'] == row['anti1']]['group'].values
+        #handle case when name not found
+        if antiDF['anti1group'][i].size == 0:
+            antiDF['anti1group'][i] = [0]
+        antiDF['anti1group'][i] = antiDF['anti1group'][i][0]
+
+    #move anti-requests in antiDF
+    groups = list(range(1,num_groups+1))
+    antiDF['group'] = antiDF.apply(lambda x: np.random.choice(list(set(groups) - {(antiDF['anti1group'][7])}), 1)[0] if x['group'] == x['anti1group'] else x['group'], axis=1)
+
+    #apply changes to entire dataframe
+    anti_free_df = df.copy()
+
+    for i, row in antiDF.iterrows():
+        anti_free_df['group'][i] = antiDF['group'][i]
+
+    anti_free_df = anti_free_df.drop(['name'], axis = 1)
+
+    return anti_free_df
 
 
 def create_student_groups(path):
