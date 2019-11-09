df663cafea3aaef14ecf9e6c6ea4d6d3651a9c5a
Markus Hamann <mhamann@edu.uwaterloo.ca>
2019-11-09 17:33:50 -0500

    Update sortleadersandstudents.py
    Changed last error and put code in correct spot

--- sortleadersandstudents.py
+++ sortleadersandstudents.py
@@ -2,78 +2,133 @@ import numpy as np
 import pandas as pd 
 import os
 
-def create_student_groups(first_year_data, leader_summary):
+def create_leader_groups(path):
+    inputdf = pd.read_csv(path)
 
-    #read in data from CSV file 
-    data = pd.read_csv(first_year_data)
-    leaders = pd.read_csv(leader_summary)
+    num_groups = 18
     
-    data['gender'] = data['gender'].replace('A','F')
-    data['gender'] = data['gender'].replace('U','F')
-    
+    #replace "U" and "A" genders with a random choice of {"F", "M"}
+    genders = ["M", "F"]
+    inputdf['gender'] = inputdf['gender'].replace("U", np.random.choice(genders, 1)[0])
+    inputdf['gender'] = inputdf['gender'].replace("A", np.random.choice(genders, 1)[0])
+
     #program bins
     arch = ["AE", "ARCH"]
     civ = ["CIVE", "GEO", "ENV"]
     ece = ["COMP", "ECE"]
     mech = ["MECH", "TRON"]
-    
+
     bins = [arch, civ, ece, mech]
-    
+
     #use anonymous function to rename program to their parent programs concatenated by "/"
     for bin in bins:
-        data['program'] = data.apply(lambda x: "/".join(bin) if x['program'] in bin else x['program'], axis=1)
+        inputdf['program'] = inputdf.apply(lambda x: "/".join(bin) if x['program'] in bin else x['program'], axis=1)
 
-    program = ['AE/ARCH', 'BME', 'CHE', 'CIVE/GEO/ENV', 'COMP/ECE', 'MECH/TRON',
-        'MGTE', 'NANO', 'SE', 'SYDE']
+    df = inputdf
 
-    groups = range(1,19)
-    
-    df = data
+    #assign leaders based on rand unif distribution [1,19)
+    df['group'] = np.random.randint(1, 19, df.shape[0]) 
 
-    df['group'] = np.random.randint(1, 19, df.shape[0])
-    
-    df = df.groupby(['program', 'gender','watIam', 'first_name', 'last_name', 'email', 'id']).size().reset_index(name='counts')
-    
-    #teamdf = pd.DataFrame(columns=['program', 'gender', 'watIam'])
-    s = df.shape[0]-1
-    #need to make dynamic in future to change number of programs
-    j=0
-    k=0
-    n=1
-    numberofprograms = 11
-    StudentFinaldf = pd.DataFrame(columns=['program', 'gender', 'watIam', 'first_name', 'last_name', 'email', 'id', 'team number'])
-    teamdf = pd.DataFrame(columns=['program', 'gender', 'watIam', 'first_name', 'last_name', 'email', 'id'])
-    for n in range(1,numberofprograms):
-        teamdf = pd.DataFrame(columns=['program', 'gender', 'watIam', 'first_name', 'last_name', 'email', 'id'])
-        while df.iat[j,0] == df.iat[j+1,0]:
-            teamdf.loc[j] = [df.iat[k,0],df.iat[k,1],df.iat[k,2],df.iat[k,3],df.iat[k,4],df.iat[k,5],df.iat[k,6]]
-            j=j+1
-            k=k+1
-            if j == df.shape[0]-1:
-                break
-        teamdf.loc[j] = [df.iat[k,0],df.iat[k,1],df.iat[k,2],df.iat[k,3],df.iat[k,4],df.iat[k,5],df.iat[k,6]]
-        j=j+1
-        k=k+1
+    #anti requests
+    df['name'] = df['last_name'] + ", " + df['first_name']
 
-        h=0
-        availteamsdf = pd.DataFrame(columns=['team number'])
-        for i, row in leaders.iterrows():
-            if row['program'] == teamdf.iat[0,0]:
-                availteamsdf.loc[h] = row['group']
-                h=h+1
-        l=0
-        m=0
-        studentAssigndf = pd.DataFrame(columns=['team number'])        
-        for l, row in teamdf.iterrows():
-            studentAssigndf.loc[l] = availteamsdf.iat[m,0]
-            if m == availteamsdf.shape[0]-1:
-                m=0
-            else:
-                m=m+1
+    antiDF = df[df['anti1'].notnull()]
+    antiDF['anti1group'] = ""
 
-        mergeddf = pd.concat([teamdf, studentAssigndf], axis = 1)
+    #get the group that your anti request belongs
+    for i, row in antiDF.iterrows():
+        #searches df for name and fetches their respective group
+        antiDF['anti1group'][i] = df[df['name'] == row['anti1']]['group'].values
+        #handle case when name not found
+        if antiDF['anti1group'][i].size == 0:
+            antiDF['anti1group'][i] = [0]
+        antiDF['anti1group'][i] = antiDF['anti1group'][i][0]
 
-        StudentFinaldf = pd.concat([StudentFinaldf, mergeddf], axis = 0)
-    StudentFinaldf.to_csv('StudentSummary.csv')
-    return StudentFinaldf
+    #move anti-requests in antiDF
+    groups = list(range(1,num_groups+1))
+    antiDF['group'] = antiDF.apply(lambda x: np.random.choice(list(set(groups) - {(antiDF['anti1group'][7])}), 1)[0] if x['group'] == x['anti1group'] else x['group'], axis=1)
 
+    #apply changes to entire dataframe
+    anti_free_df = df.copy()
+
+    for i, row in antiDF.iterrows():
+        anti_free_df['group'][i] = antiDF['group'][i]
+
+    anti_free_df = anti_free_df.drop(['name'], axis = 1)
+
+    return anti_free_df
+
+ def create_student_groups(first_year_data, leader_summary):
+
+     #read in data from CSV file 
+     data = pd.read_csv(first_year_data)
+     leaders = pd.read_csv(leader_summary)
+
+     data['gender'] = data['gender'].replace('A','F')
+     data['gender'] = data['gender'].replace('U','F')
+
+     #program bins
+     arch = ["AE", "ARCH"]
+     civ = ["CIVE", "GEO", "ENV"]
+     ece = ["COMP", "ECE"]
+     mech = ["MECH", "TRON"]
+
+     bins = [arch, civ, ece, mech]
+
+     #use anonymous function to rename program to their parent programs concatenated by "/"
+     for bin in bins:
+         data['program'] = data.apply(lambda x: "/".join(bin) if x['program'] in bin else x['program'], axis=1)
+
+     program = ['AE/ARCH', 'BME', 'CHE', 'CIVE/GEO/ENV', 'COMP/ECE', 'MECH/TRON',
+         'MGTE', 'NANO', 'SE', 'SYDE']
+
+     groups = range(1,19)
+
+     df = data
+
+     df['group'] = np.random.randint(1, 19, df.shape[0])
+
+     df = df.groupby(['program', 'gender','watIam', 'first_name', 'last_name', 'email', 'id']).size().reset_index(name='counts')
+
+     #teamdf = pd.DataFrame(columns=['program', 'gender', 'watIam'])
+     s = df.shape[0]-1
+     #need to make dynamic in future to change number of programs
+     j=0
+     k=0
+     n=1
+     numberofprograms = 11
+     StudentFinaldf = pd.DataFrame(columns=['program', 'gender', 'watIam', 'first_name', 'last_name', 'email', 'id', 'team number'])
+     teamdf = pd.DataFrame(columns=['program', 'gender', 'watIam', 'first_name', 'last_name', 'email', 'id'])
+     for n in range(1,numberofprograms):
+         teamdf = pd.DataFrame(columns=['program', 'gender', 'watIam', 'first_name', 'last_name', 'email', 'id'])
+         while df.iat[j,0] == df.iat[j+1,0]:
+             teamdf.loc[j] = [df.iat[k,0],df.iat[k,1],df.iat[k,2],df.iat[k,3],df.iat[k,4],df.iat[k,5],df.iat[k,6]]
+             j=j+1
+             k=k+1
+             if j == df.shape[0]-1:
+                 break
+         teamdf.loc[j] = [df.iat[k,0],df.iat[k,1],df.iat[k,2],df.iat[k,3],df.iat[k,4],df.iat[k,5],df.iat[k,6]]
+         j=j+1
+         k=k+1
+
+         h=0
+         availteamsdf = pd.DataFrame(columns=['team number'])
+         for i, row in leaders.iterrows():
+             if row['program'] == teamdf.iat[0,0]:
+                 availteamsdf.loc[h] = row['group']
+                 h=h+1
+         l=0
+         m=0
+         studentAssigndf = pd.DataFrame(columns=['team number'])        
+         for l, row in teamdf.iterrows():
+             studentAssigndf.loc[l] = availteamsdf.iat[m,0]
+             if m == availteamsdf.shape[0]-1:
+                 m=0
+             else:
+                 m=m+1
+
+         mergeddf = pd.concat([teamdf, studentAssigndf], axis = 1)
+
+         StudentFinaldf = pd.concat([StudentFinaldf, mergeddf], axis = 0)
+     StudentFinaldf.to_csv('StudentSummary.csv')
+     return StudentFinaldf
