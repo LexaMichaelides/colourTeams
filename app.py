d5936b50cbdad04c3b9e97ea50ffc040e473d1af
Faraaz-Ziaee <37664911+Faraaz-Ziaee@users.noreply.github.com>
2019-11-10 19:12:18 -0500

    Upload Page (#22)
    * Update upload.html
    
    Added button to upload summary of sorted leader data.
    
    * Update app.py
    
    * Update app.py
    
    * Updated front-end
    
    Updated the layout of the upload page and linked it to the backend
    
    * Integrated front-end with algo
    
    * Update upload.html
    
    * integrated algo with download
    
    * Update app.py
    
    * Renamed "Student" to "First Year"

--- app.py
+++ app.py
@@ -1,54 +1,47 @@
 import os
 import pandas as pd
 import sortleadersandstudents as algo
-
 from flask import Flask, render_template, request, redirect, flash, send_file
 from werkzeug.utils import secure_filename
-
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 results_path = os.path.join(app.root_path, 'results', 'sorted_output.csv')
 
-
-def get_data(leader_file_path, student_file_path):
-
-    while True:
-        try:
-            leader_data = algo.create_leader_groups(leader_file_path)
-        except:
-            continue
-        break
-
-    student_data = algo.create_student_groups(student_file_path)
-
-    combined_data = pd.merge(leader_data, student_data, how='outer', on=['id', 'last_name', 'first_name', 'program', 'gender', 'email', 'watIam', 'group'])
-
-    combined_data.to_csv(results_path, index = False)
-
-
 @app.route('/', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
 
-        if 'student_data' not in request.files or 'leader_data' not in request.files:
+        if 'student_data' not in request.files and 'leader_data' not in request.files and 'sorted_leader_data' not in request.files:
             flash('No file part')
             return render_template('upload.html')
 
-        student_file = request.files['student_data']
-        leader_file = request.files['leader_data']
+        elif 'leader_data' in request.files:
+            leader_file = request.files['leader_data']
 
-        if student_file.filename == '' or leader_file.filename == '' :
-            flash('No Selected File')
-            return render_template('upload.html')
+            if leader_file.filename == '':
+                flash('No Selected File')
+                return render_template('upload.html')
 
-        else:
+            else:
+                result = algo.create_leader_groups(leader_file)
+                result.to_csv(results_path, index = False)
+                return redirect('/download')
 
-            get_data(leader_file, student_file)
-            return redirect('/download')
+        elif 'student_data' in request.files and 'sorted_leader_data' in request.files:
+            student_file = request.files['student_data']
+            sorted_leader_file = request.files['sorted_leader_data']
+
+            if student_file.filename == '' and leader_file.filename == '' and sorted_leader_file.filename == ''  :
+                flash('No Selected File')
+                return render_template('upload.html')
+
+            else:
+                result = algo.create_student_groups(student_file, sorted_leader_file)
+                result.to_csv(results_path, index = False)
+                return redirect('/download') 
 
     return render_template('upload.html')
-
 
 @app.route('/download', methods=['GET', 'POST'])
 def download():
@@ -56,7 +49,6 @@ def download():
         return send_file(results_path)
 
     return render_template('download.html')
-
 
 if __name__ == '__main__':
     app.run(debug=False)
