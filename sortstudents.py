1ef9834f1c874563c0476c2750e9a98ac962d5af
hcranham <55754948+hcranham@users.noreply.github.com>
2019-11-15 11:22:15 -0500

    Cleaned up First Year Algo (#29)
    * changed input and removed column
    
    changed input to sorted data, removed empty column, added comments throughout
    
    * removed code to print file at end
    
    took out the lines i was using to test code
    
    * Updtae front end to reflect changes
    
    Changed "Sorted Leader Data Summary" to "Sorted Leader Data"" to reflect the chenges in the first year algo

--- sortstudents.py
+++ sortstudents.py
@@ -1,11 +1,15 @@
 import numpy as np
 import pandas as pd
 
-def create_first_year_groups(first_year_data, leader_summary):
+def create_first_year_groups(first_year_data, leader_sorted_data):
     # read in data from CSV file
     data = pd.read_csv(first_year_data)
-    leaders = pd.read_csv(leader_summary)
-
+    leaders = pd.read_csv(leader_sorted_data)
+    
+    #create leader summary
+    leaders = leaders.groupby(['group', 'program']).size().reset_index(name='counts')
+    
+    #replace genders to be F
     data['gender'] = data['gender'].replace('A', 'F')
     data['gender'] = data['gender'].replace('U', 'F')
 
@@ -24,26 +28,27 @@ def create_first_year_groups(first_year_data, leader_summary):
     program = ['AE/ARCH', 'BME', 'CHE', 'CIVE/GEO/ENV', 'COMP/ECE', 'MECH/TRON',
                'MGTE', 'NANO', 'SE', 'SYDE']
 
-    groups = range(1, 19)
-
     df = data
+    df = df.groupby(['program', 'gender', 'watIam', 'first_name', 'last_name', 'email', 'id']).size().reset_index(name='counts')
 
-    df['group'] = np.random.randint(1, 19, df.shape[0])
-
-    df = df.groupby(['program', 'gender', 'watIam', 'first_name', 'last_name', 'email', 'id']).size().reset_index(
-        name='counts')
-
+    #set counter variables
     s = df.shape[0] - 1
-
-    # need to make dynamic in future to change number of programs
     j = 0
     k = 0
     n = 1
     numberofprograms = 11
+
+    #create dataframe for final list of students
     StudentFinaldf = pd.DataFrame(
-        columns=['program', 'gender', 'watIam', 'first_name', 'last_name', 'email', 'id', 'team number'])
+        columns=['program', 'gender', 'watIam', 'first_name', 'last_name', 'email', 'id'])
+
+    #create dataframe to be used for each program
     teamdf = pd.DataFrame(columns=['program', 'gender', 'watIam', 'first_name', 'last_name', 'email', 'id'])
+    
+    #loop through each program and assign teams
     for n in range(1, numberofprograms):
+        
+        #make temporary df with all students in one program
         teamdf = pd.DataFrame(columns=['program', 'gender', 'watIam', 'first_name', 'last_name', 'email', 'id'])
         while df.iat[j, 0] == df.iat[j + 1, 0]:
             teamdf.loc[j] = [df.iat[k, 0], df.iat[k, 1], df.iat[k, 2], df.iat[k, 3], df.iat[k, 4], df.iat[k, 5],
@@ -57,12 +62,15 @@ def create_first_year_groups(first_year_data, leader_summary):
         j = j + 1
         k = k + 1
 
+        #get all team numbers students of a program can be assigned to based on leaders
         h = 0
         availteamsdf = pd.DataFrame(columns=['group'])
         for i, row in leaders.iterrows():
             if row['program'] == teamdf.iat[0, 0]:
                 availteamsdf.loc[h] = row['group']
                 h = h + 1
+        
+        #assign all students in one program to respective teams
         l = 0
         m = 0
         studentAssigndf = pd.DataFrame(columns=['group'])
@@ -73,7 +81,9 @@ def create_first_year_groups(first_year_data, leader_summary):
             else:
                 m = m + 1
 
+        #add column of team numbers to student list
         mergeddf = pd.concat([teamdf, studentAssigndf], axis=1)
 
+        #merge temp list of studnts to final list
         StudentFinaldf = pd.concat([StudentFinaldf, mergeddf], axis=0, sort=False)
     return StudentFinaldf
